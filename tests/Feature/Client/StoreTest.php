050632b2e655e37c6ae0bac95b32b28bd48cd611
pedzed <git@pedzed.com>
2019-12-18 15:36:34 +0100

    Added client validation with tests

--- tests/Feature/Client/StoreTest.php
+++ tests/Feature/Client/StoreTest.php
@@ -2,9 +2,8 @@
 
 namespace Tests\Feature\Client;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class StoreTest extends TestCase
 {
@@ -24,6 +23,46 @@ class StoreTest extends TestCase
                 'data' => [
                     'id' => $responseData->id,
                     'email' => $payload['email'],
+                ],
+            ])
+        ;
+    }
+
+    public function test_validates_email_type()
+    {
+        $payload = [
+            'email' => 'INVALID_EMAIL_ADDRESS',
+        ];
+
+        $this->json('POST', route('clients.store', $payload))
+            ->assertStatus(422)
+            ->assertExactJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'email' => [
+                        'The email must be a valid email address.',
+                    ],
+                ],
+            ])
+        ;
+    }
+
+    public function test_validates_email_length_too_long()
+    {
+        $maxLength = 254;
+
+        $payload = [
+            'email' => str_repeat('a', $maxLength - strlen('_long@email.com') + 1) . '_long@email.com',
+        ];
+
+        $this->json('POST', route('clients.store', $payload))
+            ->assertStatus(422)
+            ->assertExactJson([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'email' => [
+                        'The email may not be greater than ' . $maxLength . ' characters.',
+                    ],
                 ],
             ])
         ;
